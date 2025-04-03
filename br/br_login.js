7d4f3dcb156537b2f8c19eb9bf2f20fbc047cbbe
tnal465 <82003265+tnal465@users.noreply.github.com>
2025-04-03 18:40:07 +0900

    Add files via upload

---
+++ br/br_login.js
@@ -0,0 +1,25 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); // 기본 폼 제출 방지
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("http://127.0.0.1:8080/api/login", {  // 백엔드 주소로 요청
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === "로그인 성공") {
+            alert("로그인 성공!");
+            // 로그인 후 이동할 페이지 설정
+            window.location.href = "main.html";
+        } else {
+            alert("로그인 실패. 아이디와 비밀번호를 확인하세요.");
+        }
+    })
+    .catch(error => console.error("Error:", error));
+});
---
+++ br/kiosk/src/main/java/com/kiosk/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.kiosk.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@CrossOrigin(origins = "http://192.168.0.17:5555")
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+    
+	@PostMapping("/login")
+	//public ResponsEntity<Map<String, Object>> postLogin(@ModelAttribute UserDTO userdto){} //db가져올때 
+	public ResponseEntity<?> login( @RequestBody Map<String, String> loginData, HttpServletRequest request) {
+		 System.out.println("로그인 요청 들어옴!"); // 디버깅용 출력
+        System.out.println("입력값: " + loginData);
+	        
+        String username = loginData.get("username");
+        String password = loginData.get("password");
+        if ("admin".equals(username) && "1234".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
+
+            return ResponseEntity.ok(Map.of("message", "로그인 성공"));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "로그인 실패"));
+	}
+
+}
